952d2c63fa00b32cc9fee6a634bacb8d2236f383
limjaein <dlawodls96@naver.com>
2024-10-15 23:38:53 +0900

    서비스 개발

--- src/main/kotlin/com/jaein/coopy/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/jaein/coopy/presentation/service/PresentationService.kt
@@ -1,7 +1,26 @@
 package com.jaein.coopy.presentation.service
 
+import com.jaein.coopy.presentation.dto.IngredientDTO
+import com.jaein.coopy.presentation.dto.RecipeDTO
+import com.jaein.coopy.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PresentationService {
+class PresentationService (
+    private val presentationRepository: PresentationRepository
+){
+    @Transactional(readOnly = true)
+    fun getRecipes(): List<RecipeDTO> {
+        val recipes = presentationRepository.getRecipes()
+
+        return recipes.map { RecipeDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getIngredients(): List<IngredientDTO> {
+        val ingredients = presentationRepository.getIngredients()
+
+        return ingredients.map { IngredientDTO(it) }
+    }
 }
